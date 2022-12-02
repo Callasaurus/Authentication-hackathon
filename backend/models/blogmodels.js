a480572598b0444f47baedc5d17aa83262339067
Callasaurus <c.sullivandrage@outlook.com>
2022-12-02 15:09:52 +0000

    backend

--- backend/models/blogmodels.js
+++ backend/models/blogmodels.js
@@ -1,14 +1,20 @@
 import query from "../db/index.js";
 
 async function getBlogPosts(email) {
-  const allBlogPosts = await query(`SELECT * FROM users INNER JOIN posts ON posts.post_id = users.user_id WHERE email ILIKE $1`, [email]);
+  const allBlogPosts = await query(`SELECT * FROM users INNER JOIN posts ON posts.user_id = users.user_id WHERE user_email ILIKE $1`, [email]);
   return allBlogPosts.rows;
 }
 
 async function getAllBlogPosts() {
+  // const emailCheck = await query(`SELECT * FROM users WHERE user_email ILIKE $1`, [email]);
+  // if (emailCheck.rows > 0) {
+  // const allBlogPosts = await query(`SELECT * FROM posts`);
+  // return allBlogPosts.rows;
+  // }
   const allBlogPosts = await query(`SELECT * FROM posts`);
   return allBlogPosts.rows;
 }
+
 export {
   getBlogPosts,
   getAllBlogPosts
