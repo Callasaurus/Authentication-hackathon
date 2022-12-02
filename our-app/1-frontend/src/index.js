b8deb6a02505d19efe0ae3c09fcfe730f8d7812a
Callasaurus <c.sullivandrage@outlook.com>
2022-12-02 10:19:18 +0000

    authenitcation done

--- our-app/1-frontend/src/index.js
+++ our-app/1-frontend/src/index.js
@@ -4,14 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-7jn25ilb3lgigzb7.uk.auth0.com"
+    clientId="dFoBIi6yt2iLtMZ3PyTTOYddVb1y3TiK"
+    redirectUri={window.location.origin}
+    audience="https://dev-7jn25ilb3lgigzb7.uk.auth0.com/api/v2/"
+    scope="read:current_user update:current_user_metadata"
+  >
+   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Auth0Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
